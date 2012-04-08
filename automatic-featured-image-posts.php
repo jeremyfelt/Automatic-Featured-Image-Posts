9a22a50300f5976251ff9f93b4581d12168627cc
Jeremy Felt <jeremy.felt@gmail.com>
2012-04-07 18:08:55 -0700

    Experimenting with adding the image to content
    This is turning out tougher than it's worth though as there's no perfect
    hook for 'thumbnails have been created', so any time we add an image to
    the content, it's the full size image. At this point I think I'm going to
    move this stuff to a new branch for development, then tie up the new post
    format feature and call it a day.

--- automatic-featured-image-posts.php
+++ automatic-featured-image-posts.php
@@ -88,7 +88,12 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 	}
 
 	/**
-	 * Callback function to display main settings view for Automatic Featred Image Posts
+	 * Callback function to display main settings view for Automatic Featured Image Posts
+	 *
+	 * Additional checks are now implemented for post-formats and post-thumbnails. If a user's theme
+	 * does not support post thumbnails (featured images), we output a warning so that they are aware
+	 * of the possible incompatibility. If a user's theme supports post formats, we give an extra option
+	 * allowing them to choose which format to publish under.
 	 */
 	public function view_settings() {
 		?>
@@ -96,8 +101,8 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 			<div class="icon32" id="icon-options-general"></div>
 			<h2><?php _e( 'Automatic Featured Image Posts', 'automatic-featured-image-posts' ); ?></h2>
 			<h3><?php _e( 'Overview', 'automatic-featured-image-posts' ); ?></h3>
-			<p style="margin-left: 12px;max-width: 640px;"><?php _e('The default post status is set to <strong>publish</strong> by default. This means that as soon as you upload a new image through any interface in WordPress, a new post will appear with that image assigned as the featured image.', 'automatic-featured-image-posts' ); ?></p>
-			<p style="margin-left: 12px;max-width: 640px;"><?php _e('The default post type is set to the most familiar WordPress post type, <strong>post</strong>. Other custom post types installed on your site have been automatically detected and will appear in the drop down menu as options. Note that these custom post types should have support for featured images, or they may not appear as you would like.', 'automatic-featured-image-posts' ); ?></p>
+			<p style="margin-left: 12px;max-width: 640px;"><?php _e('The default <strong>post status</strong> is set to <strong>publish</strong> by default. This means that as soon as you upload a new image through any interface in WordPress, a new post will appear with that image assigned as the featured image.', 'automatic-featured-image-posts' ); ?></p>
+			<p style="margin-left: 12px;max-width: 640px;"><?php _e('The default <strong>post type</strong> is set to the most familiar WordPress post type, <strong>Post</strong>. Other custom post types registered by your theme and installed plugins have been automatically detected and will also appear in the drop down menu as options. Note that these custom post types should have support for featured images, or they may not appear as you would like.', 'automatic-featured-image-posts' ); ?></p>
 			<?php if ( current_theme_supports( 'post-formats' ) ) : ?><p style="margin-left: 12px;max-width: 640px;"><?php _e( 'Your theme supports <strong>post formats</strong>, so an extra option is available to set which format an automatically created post will be set to. The default is <strong>Standard</strong>. Note that the final display will depend on what is provided by your theme.', 'automatic-featured-image-posts' ); ?></p><?php endif; ?>
 			<?php if ( ! current_theme_supports( 'post-thumbnails' ) ) : ?><div class="error" style="margin-left: 12px;max-width: 640px;"><?php _e( '<strong>PLEASE NOTE:</strong> Your current theme does <strong>NOT</strong> support featured images and thus this plugin will be severely limited. Images will be attached to posts after upload, but it may be impossible to see this until featured image support is added to your theme.', 'automatic-featured-image-posts' ); ?></div><?php endif; ?>
 			<form method="POST" action="options.php">
@@ -122,6 +127,8 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 
 		if ( current_theme_supports( 'post-formats' ) )
 			add_settings_field( 'afip_default_post_format', __( 'Default Post Format:', 'automatic-featured-image-posts' ), array( $this, 'output_default_post_format_text' ), 'afip', 'afip_section_main' );
+
+		add_settings_field( 'afip_default_image_placement', __( 'Default Image Placement:', 'automatic-featured-image-posts' ), array( $this, 'output_default_image_placement_text' ), 'afip', 'afip_section_main' );
 
 	}
 
@@ -191,6 +198,19 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 		<?php
 	}
 
+	public function output_default_image_placement_text() {
+		$afip_options = get_option( 'afip_options' );
+
+		if ( ! isset( $afip_options[ 'default_image_placement' ] ) )
+			$afip_options[ 'default_image_placement' ] = 'featured';
+		?>
+		<select id="afip_default_image_placement" name="afip_options[default_image_placement]">
+			<option value="featured" <?php selected( $afip_options[ 'default_image_placement' ], 'featured' ); ?>>Featured Image</option>
+			<option value="content" <?php selected( $afip_options[ 'default_image_placement' ], 'content' ); ?>>Post Content</option>
+		</select>
+		<?php
+	}
+
 	/**
 	 * Validation of a drop down. Hmm. Well, if it isn't on our list, we'll force it onto our list.
 	 *
@@ -220,6 +240,9 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 
 		if ( ! in_array( $input[ 'default_post_format' ], $valid_post_format_options ) )
 			$input[ 'default_post_format' ] = 'standard';
+
+		if ( ! in_array( $input[ 'default_image_placement' ], array( 'featured', 'content' ) ) )
+			$input[ 'default_image_placement' ] = 'featured';
 
 		return $input;
 	}
@@ -278,9 +301,15 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 		$new_post_date = date( 'Y-m-d H:i:s' );
 		$current_user = wp_get_current_user();
 
+		/* Add content, update attachment with post parent post_status = inherit, post_parent = ID*/
+		if ( isset( $afip_options[ 'default_image_placement' ] ) && 'content' == $afip_options[ 'default_image_placement' ] )
+			$new_post_content = wp_get_attachment_image( $post_id, 'medium' );
+		else
+			$new_post_content = '';
+
 		$new_post_id = wp_insert_post( array(
 			'post_title' => get_the_title( $post_id ),
-			'post_content' => '',
+			'post_content' => $new_post_content,
 			'post_status' => $afip_options[ 'default_post_status' ],
 			'post_author' => $current_user->ID,
 			'post_date' => $new_post_date,
@@ -291,8 +320,11 @@ class Automatic_Featured_Image_Posts_Foghlaim {
 		if ( isset( $afip_options[ 'default_post_format' ] ) && 'standard' != $afip_options[ 'default_post_format' ] )
 			set_post_format( $new_post_id, $afip_options[ 'default_post_format' ] );
 
-		/*  Assign the featured image */
-		update_post_meta( $new_post_id, '_thumbnail_id', $post_id );
+		/* Add image as a featured image to the new post */
+		if ( isset( $afip_options[ 'default_image_placement' ] ) && 'featured' == $afip_options[ 'default_image_placement' ] )
+			update_post_meta( $new_post_id, '_thumbnail_id', $post_id );
+
+		wp_update_post( array( 'ID' => $post_id, 'post_parent' => $new_post_id, 'post_status' => 'inherit' ) );
 	}
 }
 new Automatic_Featured_Image_Posts_Foghlaim();
