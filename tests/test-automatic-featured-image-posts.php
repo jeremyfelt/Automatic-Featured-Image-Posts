d8e5d2ca52d67142341b496d5bf279e0ec654b5a
vagrant <vagrant@precise32-dev>
2013-04-27 22:57:46 +0000

    Remove sample file and setup true test file

---
+++ tests/test-automatic-featured-image-posts.php
@@ -0,0 +1,13 @@
+<?php
+
+class AutomaticFeaturedImagePostsTest extends WP_UnitTestCase {
+
+	/**
+	 * I'm here and I've loaded successfully.
+	 */
+	function testExist() {
+		$this->assertTrue( true );
+	}
+
+}
+
