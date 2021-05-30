ba915a2441928c1fa3953b8bc3aa167ccc3e7ba7
jeremyfelt <jeremy.felt@gmail.com>
2021-05-30 12:26:57 -0700

    Add boilerplate oh-no-not-php5.6 stuff

--- automatic-featured-image-posts.php
+++ automatic-featured-image-posts.php
@@ -27,4 +27,24 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// This plugin, like WordPress, requires PHP 5.6 and higher.
+if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+	add_action( 'admin_notices', 'automatic_featured_image_posts_admin_notice' );
+	/**
+	 * Display an admin notice if PHP is not 5.6.
+	 */
+	function automatic_featured_image_posts_admin_notice() {
+		echo '<div class=\"error\"><p>';
+		echo __( 'The Automatic Featured Image Posts WordPress plugin requires PHP 5.6 to function properly. Please upgrade PHP or deactivate the plugin.', 'automatic-featured-image-posts' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '</p></div>';
+	}
+
+	return;
+}
+
 require_once __DIR__ . '/includes/automatic-featured-image-posts.php';
